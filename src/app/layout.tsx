5205f0aa57d481febed5169b67f7b24dcf65460d
Patryk Przybylski <darknoriss@gmail.com>
2023-08-09 05:06:02 +0200

    fix: replace boilerplate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ import { cn } from '@/lib/utils';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Boilerplate',
-  description: 'NEXTjs Boilerplate',
+  title: 'WeatherApp',
+  description: 'WeatherApp',
 };
 
 type RootLayoutProps = {
