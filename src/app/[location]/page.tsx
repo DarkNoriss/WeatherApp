b63fc7ccb260ef9c056f915f0b3b8d49c6050786
Patryk Przybylski <darknoriss@gmail.com>
2023-08-09 05:07:36 +0200

    feat: dyn metadat, display data from api, styling

--- src/app/[location]/page.tsx
+++ src/app/[location]/page.tsx
@@ -4,6 +4,9 @@ import Image from 'next/image';
 type PageProps = {
   params: { location: string };
 };
+export const generateMetadata = async ({ params }: PageProps) => {
+  return { title: params.location };
+};
 
 const Page = async ({ params }: PageProps) => {
   const querry = await axios.get(
@@ -13,10 +16,18 @@ const Page = async ({ params }: PageProps) => {
   const { location, current } = querry.data;
 
   return (
-    <div className="flex flex-col text-center">
-      <p>{location.name}</p>
-      <Image src={current.condition.icon} alt="weather icon" />
-      <p>{current.temp_c}</p>
+    <div className="flex flex-col items-center space-y-2 text-center text-2xl font-bold">
+      <span className="text-6xl">{location.name}</span>
+      <span>{location.country}</span>
+      <span>{location.localtime}</span>
+      <Image
+        src={`https:${current.condition.icon}`}
+        alt="weather icon"
+        width={96}
+        height={96}
+      />
+      <span className="">{current.temp_c}°C</span>
+      <span className="">{current.condition.text}</span>
     </div>
   );
 };
