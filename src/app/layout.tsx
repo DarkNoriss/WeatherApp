6c8263b8d6d36079a1b4509a98d04032a56b5be5
Patryk Przybylski <darknoriss@gmail.com>
2023-08-09 03:02:32 +0200

    feat: main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import '@/styles/global.css';
 
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import type { FC } from 'react';
 
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
 import { cn } from '@/lib/utils';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -17,11 +18,17 @@ type RootLayoutProps = {
   children: React.ReactNode;
 };
 
-const RootLayout: FC<RootLayoutProps> = ({ children }) => {
+const RootLayout = async ({ children }: RootLayoutProps) => {
   return (
     <html lang="en" className={cn('bg-gray-950', inter.className)}>
-      <body className="bg-gray-950 text-gray-200">
-        <main className="container mx-auto flex">{children}</main>
+      <body className=" bg-gray-950 text-gray-200">
+        <main className="container relative mx-auto flex min-h-screen flex-col">
+          <Header />
+          <div className="flex grow items-center justify-center">
+            {children}
+          </div>
+          <Footer />
+        </main>
       </body>
     </html>
   );
