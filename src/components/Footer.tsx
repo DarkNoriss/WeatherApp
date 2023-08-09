104fe2e761f62b5d6c0b067026377b5c94cc57a3
Patryk Przybylski <darknoriss@gmail.com>
2023-08-09 05:06:50 +0200

    feat: icon, text size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,22 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="shrink-0 text-center">
+    <footer className="shrink-0 py-1">
       <Link
         href="https://github.com/DarkNoriss/"
         target="_blank"
         rel="noopener noreferrer"
-        className=" hover:text-blue-400"
+        className="flex flex-row items-center justify-center space-x-1 hover:brightness-75"
       >
-        <p>DarkNoriss</p>
+        <Image
+          src="/github-mark-white.png"
+          alt="GitHub icon"
+          height={16}
+          width={16}
+        />
+        <span className="text-lg">DarkNoriss</span>
       </Link>
     </footer>
   );
