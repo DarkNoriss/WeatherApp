cfcb03609a073fe3e1a22101e570f0707b8f8dbc
Patryk Przybylski <darknoriss@gmail.com>
2023-08-09 02:59:37 +0200

    feat: add author

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,18 @@
+import Link from 'next/link';
+
 const Footer = () => {
-  return <div>Footet</div>;
+  return (
+    <footer className="shrink-0 text-center">
+      <Link
+        href="https://github.com/DarkNoriss/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className=" hover:text-blue-400"
+      >
+        <p>DarkNoriss</p>
+      </Link>
+    </footer>
+  );
 };
 
 export default Footer;
